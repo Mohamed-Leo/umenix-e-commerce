95022d8adc0a501ac2fecb36b315003d1497f0e8
salma <salma_ahmed39@ymail.com>
2024-08-31 17:12:34 +0200

    feat:add hero section

--- uimenix-front/src/App.tsx
+++ uimenix-front/src/App.tsx
@@ -1,11 +1,27 @@
+import Hero from "./components/home pages/Hero";
 import Layout from "./components/layouts/Layout";
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
+
+import { useEffect } from "react";
 
 
 function App() {
-
+  useEffect(()=>{
+    AOS.init(
+     {
+       offset:100,
+       duraton:800,
+       easing:"ease-in-sine",
+       delay:100,
+     }
+    )
+    AOS.refresh();
+   },[]);
   return (
     <>
       <Layout />
+      <Hero/>
     </>
   )
 }
--- uimenix-front/tailwind.config.js
+++ uimenix-front/tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: "480px",
+        ss: "620px",
+        sm: "768px",
+        md: "1060px",
+        lg: "1200px",
+        xl: "1700px",
+      },
+    },
   },
   plugins: [],
 }
